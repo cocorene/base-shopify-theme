a9c9027339f573ba4c1ce25d5799401567f58d91
estrattonbailey <ericstrattonbailey@gmail.com>
2016-04-07 17:17:54 -0400

    Logging in files.js

--- tasks/files.js
+++ tasks/files.js
@@ -5,6 +5,7 @@ var copy = require('copy');
 var watch = require('gulp-watch');
 var gutil = require('gulp-util');
 var prettyHrtime = require('pretty-hrtime');
+var startTime, filePath;
 
 var assets = [
   './src/assets/*.png',
@@ -13,38 +14,53 @@ var assets = [
 ]
 
 gulp.task('files:copy', function(){
-  copy('./src/layouts/*.liquid', './dist/layout', log)
-  copy('./src/templates/**/*.liquid', './dist/templates', log)
-  copy('./src/snippets/**/*.liquid', './dist/snippets', log)
-  copy(assets, './dist/assets', log)
+  copy('./src/layouts/*.liquid', './dist/layout', log.end)
+  copy('./src/templates/**/*.liquid', './dist/templates', log.end)
+  copy('./src/snippets/**/*.liquid', './dist/snippets', log.end)
+  copy(assets, './dist/assets', log.end)
 });
 
 gulp.task('files:watch', ['files:copy'], function(){
   watch('./src/layout/*.liquid', function(vinyl){
-    copy('./src/layouts/*.liquid', './dist/layout', log)
+    log.filepath = 'layouts/'+path.basename(vinyl.path);
+    log.start();
+    copy('./src/layouts/*.liquid', './dist/layout', log.end)
   });
   watch('./src/templates/**/*.liquid', function(){
-    copy('./src/templates/**/*.liquid', './dist/templates', log)
+    copy('./src/templates/**/*.liquid', './dist/templates', log.end)
   });
   watch('./src/snippets/**/*.liquid', function(){
-    copy('./src/snippets/**/*.liquid', './dist/snippets', log)
+    copy('./src/snippets/**/*.liquid', './dist/snippets', log.end)
   });
   watch(assets, function(vinyl){
     if (vinyl.event === 'unlink'){
-      del('../dist/assets/'+path.basename(vinyl.path), log);
+      del('../dist/assets/'+path.basename(vinyl.path), log.end);
     } else {
-      copy(assets, './dist/assets', log)
+      copy(assets, './dist/assets', log.end)
     }
   });
 });
 
-function log(err, file){
-  var taskTime = process.hrtime(startTime);
-  var prettyTime = prettyHrtime(taskTime);
+// function log(err, file){
+//   var taskTime = process.hrtime(startTime);
+//   var prettyTime = prettyHrtime(taskTime);
 
-  if (err) {
-    gutil.log('Error: ', gutil.colors.red(err));
+//   if (err) {
+//     gutil.log('Error: ', gutil.colors.red(err));
+//   }
+
+//   gutil.log('Copied', gutil.colors.green('files'), 'in', gutil.colors.magenta(prettyTime));
+// }
+
+var log = {
+  filepath: '',
+  start: function(filepath) {
+    startTime = process.hrtime();
+    gutil.log('Copying', gutil.colors.green(this.filepath));
+  },
+  end: function(filepath) {
+    var taskTime = process.hrtime(startTime);
+    var prettyTime = prettyHrtime(taskTime);
+    gutil.log('Copied', gutil.colors.green(log.filepath), 'in', gutil.colors.magenta(prettyTime));
   }
-
-  gutil.log('Copied', gutil.colors.green('files'), 'in', gutil.colors.magenta(prettyTime));
-}
+};

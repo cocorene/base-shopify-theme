0962565a1b4ae714d55713110b44ef82ed2323dc
estrattonbailey <ericstrattonbailey@gmail.com>
2016-04-09 11:27:11 -0400

    Comment files task

--- tasks/files.js
+++ tasks/files.js
@@ -1,10 +1,18 @@
-var path = require('path');
-var del = require('delete');
-var gulp = require('gulp');
-var watch = require('gulp-watch');
-var flatten = require('gulp-flatten');
-var logger = require('./util/log');
+var gulp = require('gulp'),
+    path = require('path'),
+    watch = require('gulp-watch'),
+    del = require('delete'),
+    flatten = require('gulp-flatten'),
+    logger = require('./util/log');
 
+/**
+ * DEFAULT
+ * Source globs and dest paths
+ *
+ * TODO
+ * [Assets] will need to be expanded on to
+ * accommodate more file types
+ */
 var files = {
   layout: {
     src: './src/layout/*.liquid',
@@ -28,14 +36,23 @@ var files = {
   }
 }
 
+/**
+ * DEFAULT Copy Task
+ * Copies all files from /src to /dist
+ */
 gulp.task('files:copy', function(){
-  logger.start('all files', 'Copying');
+  logger.start('all files', 'Copying'); // start log
   copy(files.layout.src, files.layout.dest, logger.end)
   copy(files.templates.src, files.templates.dest, logger.end)
   copy(files.snippets.src, files.snippets.dest, {flatten: true}, logger.end)
   copy(files.assets.src, files.assets.dest, logger.end)
 });
 
+/**
+ * DEV Copy Task
+ * Watches filepaths for changes and 
+ * copies changed files.
+ */
 gulp.task('files:watch', ['files:copy'], function(){
   watch(files.layout.src, function(vinyl){
     processFiles(vinyl, 'layouts')
@@ -51,6 +68,41 @@ gulp.task('files:watch', ['files:copy'], function(){
   });
 });
 
+/**
+ * Watch Handler
+ * Scrubs event type (change|unlink) and
+ * either copies or deletes file
+ *
+ * @param {stream} vinyl The Vinyl stream returned from watch()
+ * @param {string} type The type of file being processed
+ * @param {object} opts Options to pass to copy() task (optional)
+ */
+function processFiles(vinyl, type, opts){
+  var filename = path.basename(vinyl.path);
+
+  opts = opts || {};
+
+  if (vinyl.event === 'unlink'){
+    logger.start(type+'/'+filename, 'Deleting'); // start log
+    del(__dirname+'/../dist/'+type+'/'+filename, {force: true}, function(err){
+      if (err) throw err;
+      logger.end();
+    });
+  } else {
+    logger.start(type+'/'+filename, 'Copying'); // start log
+    copy(files[type].src, files[type].dest, opts, logger.end)
+  }
+}
+
+/**
+ * MAIN Copy Function
+ * A wrapper for gulp.(src|dest)
+ *
+ * @param {string|array} files The default glob patterns in the files object
+ * @param {string} dest The default dist path in the files object
+ * @param {object} opts Options (optional)
+ * @param {function} cb The logger callback function
+ */
 function copy(files, dest, opts, cb){
   if (typeof opts === 'function'){
     cb = opts; 
@@ -66,23 +118,4 @@ function copy(files, dest, opts, cb){
   }
 
   cb();
-}
-
-function processFiles(vinyl, type, opts){
-  var filename = path.basename(vinyl.path);
-
-  opts = opts || {};
-
-  if (vinyl.event === 'unlink'){
-    logger.start(type+'/'+filename, 'Deleting');
-    del(__dirname+'/../dist/'+type+'/'+filename, {force: true}, function(err){
-      if (err) throw err;
-      logger.end();
-    });
-  } 
-  
-  else {
-    logger.start(type+'/'+filename, 'Copying');
-    copy(files[type].src, files[type].dest, opts, logger.end)
-  }
 }

149d64fbedd31b02455d729ef801fcd7c3aeef4a
estrattonbailey <ericstrattonbailey@gmail.com>
2016-04-12 14:51:52 -0400

    Add shopify theme gem and dist/ check to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var shell = require('gulp-shell');
 
 require('./tasks/sass');
 require('./tasks/files');
@@ -6,6 +7,8 @@ require('./tasks/browserify');
 require('./tasks/jscs');
 require('./tasks/jshint');
 
-gulp.task('dev', ['sass:dev', 'js:dev', 'files:watch']);
+gulp.task('shopify', shell.task(['if [ -d dist/ ]; then cp config.yml dist/ && cd dist/ && theme watch; else mkdir dist/ && cp config.yml dist/ && cd dist/ && theme watch; fi']));
+
+gulp.task('dev', ['sass:dev', 'js:dev', 'files:watch', 'shopify']);
 
 gulp.task('default', ['sass', 'js', 'files:copy']);

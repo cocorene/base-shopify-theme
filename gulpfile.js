daaf9cc584cf54287e529ca4fdb46afa5904caa4
estrattonbailey <ericstrattonbailey@gmail.com>
2016-04-12 17:32:12 -0400

    Add init task to configure config.yml

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,11 @@
 var gulp = require('gulp');
-var shell = require('gulp-shell');
 
+require('./tasks/init');
 require('./tasks/sass');
 require('./tasks/files');
 require('./tasks/browserify');
 require('./tasks/jscs');
 require('./tasks/jshint');
-
-gulp.task('shopify', shell.task(['if [ -d dist/ ]; then cp config.yml dist/ && cd dist/ && theme watch; else mkdir dist/ && cp config.yml dist/ && cd dist/ && theme watch; fi']));
 
 gulp.task('dev', ['sass:dev', 'js:dev', 'files:watch', 'shopify']);
 

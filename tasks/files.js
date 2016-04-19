ed8268782a548ab607a1ac42390a687ee3da1c18
Thomas Nguyen-Huu <thomas.nguyen-huu@barrelny.com>
2016-04-18 20:26:11 -0400

    Add basic styles
    - Clear theme html structure.
    - Add header and footer style (work in progress).
    - Add transitions map.
    - Add layout variables.

--- tasks/files.js
+++ tasks/files.js
@@ -1,10 +1,10 @@
-var gulp = require('gulp'),
-    path = require('path'),
-    del = require('delete'),
-    flatten = require('gulp-flatten'),
-    processLog = require('./util/log'),
-    concat = require('gulp-concat'),
-    wrap = require('gulp-wrapper');
+var gulp = require( 'gulp' ),
+    path = require( 'path' ),
+    del = require( 'delete' ),
+    flatten = require( 'gulp-flatten' ),
+    processLog = require( './util/log' ),
+    concat = require( 'gulp-concat' ),
+    wrap = require( 'gulp-wrapper' );
 
 /**
  * DEFAULT
@@ -15,83 +15,83 @@ var gulp = require('gulp'),
  * accommodate more file types
  */
 var files = {
-  layout: {
-    src: './src/layout/*.liquid',
-    dest: './dist/layout/'
-  },
-  templates: {
-    src: './src/templates/**/*.liquid',
-    dest: './dist/templates/'
-  },
-  snippets: {
-    src: './src/snippets/**/*.liquid',
-    dest: './dist/snippets/'
-  },
-  assets: {
-    src: [
-      './src/assets/*.png',
-      './src/assets/*.jpg',
-      './src/assets/*.svg'
-    ],
-    dest: './dist/assets/'
-  },
-  config: {
-    src: [
-      './src/config/lib/general-info.json'
-    ],
-    dest: './dist/config/'
-  },
-  locales: {
-    src: './src/locales/*.json',
-    dest: './dist/locales/'
-  }
+    layout: {
+        src: './src/layout/*.liquid',
+        dest: './dist/layout/'
+    },
+    templates: {
+        src: './src/templates/**/*.liquid',
+        dest: './dist/templates/'
+    },
+    snippets: {
+        src: './src/snippets/**/*.liquid',
+        dest: './dist/snippets/'
+    },
+    assets: {
+        src: [
+            './src/assets/*.png',
+            './src/assets/*.jpg',
+            './src/assets/*.svg'
+        ],
+        dest: './dist/assets/'
+    },
+    config: {
+        src: [
+            './src/config/lib/general-info.json'
+        ],
+        dest: './dist/config/'
+    },
+    locales: {
+        src: './src/locales/*.json',
+        dest: './dist/locales/'
+    }
 }
 
 /**
  * DEFAULT Copy Task
  * Copies all files from /src to /dist
  */
-gulp.task('files:copy', function(){
-  processLog.start('all files', 'Copying'); // start log
-  copy(files.layout.src, files.layout.dest, processLog.end.bind(null,'layout'))
-  copy(files.templates.src, files.templates.dest, processLog.end.bind(null, 'templates'))
-  copy(files.snippets.src, files.snippets.dest, {flatten: true}, processLog.end.bind(null, 'snippets'))
-  copy(files.assets.src, files.assets.dest, processLog.end.bind(null, 'assets'))
-  copy(files.locales.src, files.locales.dest, processLog.end.bind(null, 'locales'))
-});
-gulp.task('config:concat', function(){
-  gulp.src(files.config.src)
-    .pipe(concat('settings_schema.json', {newLine: ','}))
-    .pipe(wrap({
-      header: '[',
-      footer: ']'
-    }))
-    .pipe(gulp.dest(files.config.dest));
-});
+gulp.task( 'files:copy', function() {
+    processLog.start( 'all files', 'Copying' ); // start log
+    copy( files.layout.src, files.layout.dest, processLog.end.bind( null, 'layout' ) )
+    copy( files.templates.src, files.templates.dest, processLog.end.bind( null, 'templates' ) )
+    copy( files.snippets.src, files.snippets.dest, { flatten: true }, processLog.end.bind( null, 'snippets' ) )
+    copy( files.assets.src, files.assets.dest, processLog.end.bind( null, 'assets' ) )
+    copy( files.locales.src, files.locales.dest, processLog.end.bind( null, 'locales' ) )
+} );
+gulp.task( 'config:concat', function() {
+    gulp.src( files.config.src )
+        .pipe( concat( 'settings_schema.json', { newLine: ',' } ) )
+        .pipe( wrap( {
+            header: '[',
+            footer: ']'
+        } ) )
+        .pipe( gulp.dest( files.config.dest ) );
+} );
 
 /**
  * DEV Copy Task
  * Watches filepaths for changes and 
  * copies changed files.
  */
-gulp.task('files:watch', ['files:copy', 'config:concat'], function(){
-  gulp.watch(files.layout.src, function(event){
-    processFiles(event, 'layout')
-  });
-  gulp.watch(files.templates.src, function(event){
-    processFiles(event, 'templates')
-  });
-  gulp.watch(files.snippets.src, function(event){
-    processFiles(event, 'snippets')
-  });
-  gulp.watch(files.assets.src, function(event){
-    processFiles(event, 'assets')
-  });
-  gulp.watch(files.locales.src, function(event){
-    processFiles(event, 'locales')
-  });
-  gulp.watch(files.config.src, ['config:concat']);
-});
+gulp.task( 'files:watch', [ 'files:copy', 'config:concat' ], function() {
+    gulp.watch( files.layout.src, function( event ) {
+        processFiles( event, 'layout' )
+    } );
+    gulp.watch( files.templates.src, function( event ) {
+        processFiles( event, 'templates' )
+    } );
+    gulp.watch( files.snippets.src, function( event ) {
+        processFiles( event, 'snippets' )
+    } );
+    gulp.watch( files.assets.src, function( event ) {
+        processFiles( event, 'assets' )
+    } );
+    gulp.watch( files.locales.src, function( event ) {
+        processFiles( event, 'locales' )
+    } );
+    gulp.watch( files.config.src, [ 'config:concat' ] );
+} );
 
 /**
  * Watch Handler
@@ -102,21 +102,21 @@ gulp.task('files:watch', ['files:copy', 'config:concat'], function(){
  * @param {string} type The type of file being processed
  * @param {object} opts Options to pass to copy() task (optional)
  */
-function processFiles(event, type, opts){
-  var filename = path.basename(event.path);
+function processFiles( event, type, opts ) {
+    var filename = path.basename( event.path );
 
-  opts = opts || {};
+    opts = opts || {};
 
-  if (event.event === 'deleted'){
-    processLog.start(type+'/'+filename, 'Deleting'); // start log
-    del(__dirname+'/../dist/'+type+'/'+filename, {force: true}, function(err){
-      if (err) throw err;
-      processLog.end();
-    });
-  } else {
-    processLog.start(type+'/'+filename, 'Copying'); // start log
-    copy(files[type].src, files[type].dest, opts, processLog.end)
-  }
+    if ( event.event === 'deleted' ) {
+        processLog.start( type + '/' + filename, 'Deleting' ); // start log
+        del( __dirname + '/../dist/' + type + '/' + filename, { force: true }, function( err ) {
+            if ( err ) throw err;
+            processLog.end();
+        } );
+    } else {
+        processLog.start( type + '/' + filename, 'Copying' ); // start log
+        copy( files[ type ].src, files[ type ].dest, opts, processLog.end )
+    }
 }
 
 /**
@@ -128,19 +128,19 @@ function processFiles(event, type, opts){
  * @param {object} opts Options (optional)
  * @param {function} cb The processLog callback function
  */
-function copy(files, dest, opts, cb){
-  if (typeof opts === 'function'){
-    cb = opts; 
-  }
+function copy( files, dest, opts, cb ) {
+    if ( typeof opts === 'function' ) {
+        cb = opts;
+    }
 
-  if (opts.flatten) {
-    gulp.src(files)
-      .pipe(flatten())
-      .pipe(gulp.dest(dest));
-  } else {
-    gulp.src(files)
-      .pipe(gulp.dest(dest));
-  }
+    if ( opts.flatten ) {
+        gulp.src( files )
+            .pipe( flatten() )
+            .pipe( gulp.dest( dest ) );
+    } else {
+        gulp.src( files )
+            .pipe( gulp.dest( dest ) );
+    }
 
-  cb();
+    cb();
 }

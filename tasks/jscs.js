3a10e8b945c6496c3ebc1e6ff1e9a841abecff23
estrattonbailey <ericstrattonbailey@gmail.com>
2016-06-06 12:00:26 -0400

    Remove unused task files, update deps, include main.js tag in theme

--- tasks/jscs.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var jscs = require('gulp-jscs');
- 
-gulp.task('jscs', function () {
-  return gulp.src(['./src/assets/js/**/*.js'])
-		.pipe(jscs({fix: true}))
-		.pipe(jscs.reporter())
-		.pipe(jscs.reporter('fail'));
-});
--- tasks/jshint.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var jshint = require('gulp-jshint');
- 
-gulp.task('jshint', function() {
-  return gulp.src(['./src/assets/js/**/*.js'])
-    .pipe(jshint())
-    .pipe(jshint.reporter());
-});

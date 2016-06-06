c1cad7ce1132ed04b228dd54ae72322ad4477c80
estrattonbailey <ericstrattonbailey@gmail.com>
2016-06-06 12:03:43 -0400

    Update init-modules script, main.js

---
+++ src/assets/js/lib/initScripts.js
@@ -0,0 +1,136 @@
+window.app = {};
+
+/**
+ * Parse data from context element attribute
+ *
+ * @param {object} el Context element
+ * @param {string} type Classification of script
+ */
+export default function(el, type){
+  var raw, sequences
+
+  window.app[type] = {}
+
+  raw = el.getAttribute('data-'+type)
+
+  sequences = raw.indexOf(',') ? raw.split(/\,\s|\,/) : raw
+
+  sequences.forEach(function(seq){
+    var _return,
+        namespace,
+        args,
+        context,
+        snippets = [],
+        params = [];
+
+    namespace = seq.split(/\s\#/)[1] || null
+
+    if (namespace) {
+      seq = seq.slice(0, seq.match(/\s\#/).index)
+    }
+
+    /**
+     * If snippet has params,
+     * we need to set them up to be
+     * passed to the script
+     */
+    if (seq.indexOf('--') > -1){
+      args = seq.split(/\s\-\-\s/)
+
+      snippets = args[1].split(/\s\|\s/)
+
+      params = [].map.call(args[0].split(/\s/), function(param){
+        return param.replace(/\'/g,'')
+      });
+    } 
+    
+    /**
+     * Otherwise, we only have one snippet,
+     * and we can just call it at that.
+     */
+    else {
+      snippets.push(seq)
+    }
+
+    /**
+     * Main function. Must be the LAST snippet
+     * passed to the attribute
+     */
+    context = snippets[snippets.length - 1]
+
+    // Remove the context snippet from the array
+    snippets.splice(snippets.length -1, 1)
+
+    // Create object to pass to init()
+    _return = {
+      context: context,
+      snippets: snippets,
+      params: params || []
+    }
+
+    /** 
+     * Run init() to fire all snippets
+     * and initialize context script
+     */
+    init(el, _return, type)
+  });
+}
+
+/**
+ * Find scripts and fire them
+ *
+ * @param {object} el DOM node
+ * @param {object} args {context: fn(), snippets: [fns()], params: []}
+ * @param {string} type Classification of script
+ */
+export function init(el, args, type){
+  var context,
+      returnData,  
+      fns = [];
+
+  /**
+   * Set default param data
+   */
+  returnData = args.params
+
+  /**
+   * Find js snippets and add to 
+   * fns array to be called next
+   */
+  try {
+    context = require(type+'s/'+args.context)
+  } catch(e) {
+    console.log(e.toString())
+  }
+
+  for (var s = 0; s < args.snippets.length; s++){
+    try {
+      fns[s] = require('lib/'+args.snippets[s])
+    } catch(e) {
+      console.log(e.toString())
+    }
+  }
+
+  /**
+   * Scrub data through utility functions
+   */
+  if(fns.length) {
+    for (var i = 0; i < fns.length; i++){
+      returnData = fns[i](args.params)
+      fns[i+1] ? fns[i+1](returnData) : null
+    }
+  }
+
+  /**
+   * Fire main snippet
+   */
+  var instance = new context(el, returnData)
+
+  if (args.namespace) {
+    window.app[type][args.namespace] = instance
+  }
+
+  if (typeof jQuery !== 'undefined'){
+    $(el).data(type, instance)
+  }
+}
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,47 +1,17 @@
-document.addEventListener( 'DOMContentLoaded', function() {
+import config from '../../package.json'
+import initScripts from './lib/initScripts'
 
-    // Each type needs to match a singular data- attribute suffix and plural equivalient of folder name 
-    var types = [ 'module', 'component' ]; 
+document.addEventListener('DOMContentLoaded', function(){
+  /**
+   * Find all specified elements
+   * in the DOM and init their javascripts
+   */
+  config.barrel.types.forEach(function(type){
+    var nodes = [].slice.call(document.querySelectorAll('[data-'+type+']'));
 
-    // Find the instances of each type
-    for ( var i = 0; i < types.length; i++ ) {
-        prepare( document.querySelectorAll( '[data-'+types[i]+']' ), types[i] );
+    for (var i = 0; i < nodes.length; i++){
+      initScripts(nodes[i], type)
     }
+  });
+});
 
-    function init( el, file, type, params ) {
-        var File;
-
-        try {
-            File = require( type + '/' + file );
-        } catch ( e ) {
-            console.log( e.toString() );
-            File = false;
-        }
-
-        // Initialize the module with the calling element.
-        if ( typeof File === 'function' ) {
-            var snippet = new File( el, params );
-
-            if ( typeof jQuery !== 'undefined' ) {
-                jQuery( el ).data( type, snippet );
-            }
-        }
-
-        return snippet;
-    }
-
-    function prepare( type, type_name ) {
-        for ( var i = 0; i < type.length; i++ ) {
-            var type_data = type[ i ].getAttribute( 'data-'+type_name );
-            var type_methods = type_data.split('|');
-            var type_params = type_methods[0];
-            if( type_methods.length > 1 ) {
-                type_methods.splice(0, 1);
-            }
-            for( var m = 0; m < type_methods.length; m++ ){
-                var return_params = init( type[ i ], type_methods[ m ].trim(), type_name+'s', type_params );
-                type_params = typeof return_params.return !== 'undefined' && return_params.return ? return_params.return() : type_params;
-            }
-        }
-    }
-} );

8e2f426cf9e92d71e5cf747f02dc10a0cd7b95d4
estrattonbailey <ericstrattonbailey@gmail.com>
2016-08-23 11:57:54 -0400

    Init Scripts:
    Update to support ES6. Support optional context.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,17 +1,7 @@
 import config from '../../../package.json'
-import initScripts from './lib/initScripts'
+import init from './lib/init'
 
 document.addEventListener('DOMContentLoaded', function(){
-  /**
-   * Find all specified elements
-   * in the DOM and init their javascripts
-   */
-  config.barrel.types.forEach(function(type){
-    var nodes = [].slice.call(document.querySelectorAll('[data-'+type+']'));
-
-    for (var i = 0; i < nodes.length; i++){
-      initScripts(nodes[i], type)
-    }
-  });
+  init()
 });
 

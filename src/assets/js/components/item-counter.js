0eaa850762c2a9b27787fec04131f702f9f4ffaa
Thomas Nguyen-Huu <thomas.nguyen-huu@barrelny.com>
2016-09-26 10:35:34 -0400

    Update Item Counter component
    - Add condition on Update Buttons existence
    - Update liquid settings for the component

--- src/assets/js/components/item-counter.js
+++ src/assets/js/components/item-counter.js
@@ -1,15 +1,17 @@
 import updown from 'up-down'
 
 export default( el ) => {
-    const counter = updown( el )
-    const initialValue = counter.value
-    const updateButton = el.parentNode.querySelector( '.js-update' )
+    const counter = updown( el );
+    const initialValue = counter.value;
+    const updateButton = el.parentNode.querySelector( '.js-update' );
 
-    counter.on( 'change', ( val ) => {
-        if( val !== initialValue ) {
-            updateButton.classList.add( 'is-active' )
-        } else if( val === initialValue ) {
-            updateButton.classList.remove( 'is-active' )
-        }
-    } )
+    if( updateButton ) {
+        counter.on( 'change', ( val ) => {
+            if( val !== initialValue ) {
+                updateButton.classList.add( 'is-active' );
+            } else if( val === initialValue ) {
+                updateButton.classList.remove( 'is-active' );
+            }
+        } );
+    }
 }

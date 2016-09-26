0ad3f354ca8a189692d5962341ebf7ae09bc61bc
Thomas Nguyen-Huu <thomas.nguyen-huu@barrelny.com>
2016-09-26 18:05:46 -0400

    Clean Product Purchase module
    - Update dynamic thumbnail Wip
    - Update dynamic price WIP

--- src/assets/js/modules/product-purchase.js
+++ src/assets/js/modules/product-purchase.js
@@ -1,30 +1,16 @@
 export default( el ) => {
     const productSelect = document.getElementById( 'productSelect' );
     const variantSelects = el.querySelectorAll( '.js-variant' );
-    const getVariants = selects => {
-        let variants = new Set();
 
-        for( let select of selects ) {
-            variants.add( select.options[ select.selectedIndex ].value );
-        }
+    for( let select of variantSelects ) {
+        select.addEventListener( 'change', ( e ) => {
+            let variants = [];
 
-        return variants;
-    }
-
-    for( let variantSelect of variantSelects ) {
-        variantSelect.addEventListener( 'change', ( e ) => {
-            let variants = getVariants( variantSelects );
-
-            for( let i = 0; i < productSelect.length; i++ ) {
-                console.log( productSelect.options );
-                // for( let variant of variants ) {
-                //     variantFound += ( option.value.contains( variant ) ) ? 1 : 0;
-                // }
-
-                // if( variantFound == variants.size() ) {
-                //     console.log( option.value );
-                // }
+            for( let select of variantSelects ) {
+                variants.push( select.options[ select.selectedIndex ].value );
             }
+
+            variants = variants.join( '-' );
         } );
     }
 }

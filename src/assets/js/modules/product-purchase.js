a55cee21e4b27a4e038540b8a0b167e0947c8b26
Thomas Nguyen-Huu <thomas.nguyen-huu@barrelny.com>
2016-09-27 11:22:29 -0400

    Update Product Purchase module
    - Fix Errors
    - Optimize Code
    - WIP dynamic URL and Thumbnails

--- src/assets/js/modules/product-purchase.js
+++ src/assets/js/modules/product-purchase.js
@@ -1,16 +1,32 @@
 export default( el ) => {
     const productSelect = document.getElementById( 'productSelect' );
     const variantSelects = el.querySelectorAll( '.js-variant' );
+    const getData = ( element, attribute ) => {
+        return element.getAttribute( attribute );
+    };
 
     for( let select of variantSelects ) {
         select.addEventListener( 'change', ( e ) => {
             let variants = [];
+            let option = null;
+            let product = new Map();
 
             for( let select of variantSelects ) {
                 variants.push( select.options[ select.selectedIndex ].value );
             }
 
             variants = variants.join( '-' );
+
+            option = productSelect.querySelector( `[data-variant="${variants}"]` );
+            option.selected = true;
+
+            product.set( 'id', getData( option, 'value' ) );
+            product.set( 'price', getData( option, 'data-price' ) );
+            product.set( 'variants', variants );
+
+            console.log( product.get( 'id' ) );
+            console.log( product.get( 'price' ) );
+            console.log( product.get( 'variants' ) );
         } );
     }
 }

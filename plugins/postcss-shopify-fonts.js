91374a7e9d6d2a3cd658c784ec5424ca613c5792
Max Rolon <max.rolon@barrelny.com>
2017-11-02 11:27:24 -0400

    Adds comment for postcss plugin

--- plugins/postcss-shopify-fonts.js
+++ plugins/postcss-shopify-fonts.js
@@ -1,3 +1,14 @@
+/**
+ * Shopify provides the ability to use liquid template tags in
+ * CSS files. However, our development workflow doesn't send
+ * CSS or JS to Shopify so we cannot compile these liquid tags.
+ * This little plugin allows fonts to use the asset_url filter,
+ * prefixing the file with a hardcoded asset CDN url. This is
+ * only used in development, not when compiling for production
+ * or staging.
+ *
+ * {{ 'file-name' | asset_url }} > //cdn.url/font-name
+ */
 const postcss = require('postcss')
 
 const trasverse = url => rule => {

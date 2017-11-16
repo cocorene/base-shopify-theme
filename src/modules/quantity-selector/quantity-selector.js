b8db9a79345b086aa560ec1d6800a68f87ce4706
Thomas Nguyen-Huu <thomas.nguyen-huu@barrelny.com>
2017-11-16 12:56:00 +0100

    Add utility library to quantity-selector module

--- src/modules/quantity-selector/quantity-selector.js
+++ src/modules/quantity-selector/quantity-selector.js
@@ -1,35 +1,8 @@
 import select from 'select-dom'
 import on from 'dom-event'
-import { set } from 'Lib/util'
+import { count, update } from 'lib/util'
 
-const elements = select.all('.js-counter')
-
-const update = (el) => {
-  if (el.disabled) {
-    for (let item of el.children) {
-      item.disabled = true
-    }
-  } else {
-    let value = parseInt(el.field.value)
-
-    el.minus.disabled = (value === el.min)
-    el.plus.disabled = (value === el.max)
-  }
-}
-
-const count = (el, btn) => {
-  if (!el.disabled) {
-    let value = parseInt(el.field.value)
-
-    if (btn === el.plus && value < el.max) value++
-    if (btn === el.minus && value > el.min) value--
-
-    el.field.value = value
-    update(el)
-  }
-}
-
-export default () => {
+export default (el) => {
   for (let el of elements) {
     if (!el.classList.contains('is-init')) {
       let field = select('.js-field', el)
@@ -57,8 +30,6 @@ export default () => {
       on(counter.field, 'blur', () => {
         update(counter)
       })
-
-      set(el, 'is-init')
     }
   }
 }

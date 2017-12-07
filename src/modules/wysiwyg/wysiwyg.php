ed31a5dae9e9bcbd1afc46c2cd35e1674566f47a
Max Rolon <max.rolon@barrelny.com>
2017-12-07 10:02:42 -0500

    Updates styleguide

--- src/modules/wysiwyg/wysiwyg.php
+++
@@ -1,8 +0,0 @@
-<?php if ( !empty( $content ) ) : ?>
-
-<section class="section wysiwyg">
-  <div class="container wysiwyg__container">
-    <?php echo apply_filters('the_content', $content); ?>
-  </div>
-</section>
-<?php endif; ?>

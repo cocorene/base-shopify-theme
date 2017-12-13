a98642f049e51d1d49a3b6cd84bf89c7ab4c37c6
Thomas Nguyen-Huu <thomas.nguyen-huu@barrelny.com>
2017-12-13 20:15:59 +0800

    Remove old image module style and JS

--- src/modules/image/image.js
+++ src/modules/image/image.js
@@ -1,10 +1,5 @@
-import select from 'select-dom'
-import on from 'dom-event'
-import { lazy, get } from 'lib/util'
+import Layzr from 'layzr.js'
 
 export default (el) => {
-  lazy(el)
-  on(window, 'resize', () => {
-    lazy(el)
-  })
+
 }

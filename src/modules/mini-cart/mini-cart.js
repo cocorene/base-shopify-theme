1b6bbd2b1b6edbd2789b22d7cc90ca252c47a4c9
Thomas Nguyen-Huu <thomas.nguyen-huu@barrelny.com>
2017-12-14 16:03:02 +0800

    Add ajax request for cart

--- src/modules/mini-cart/mini-cart.js
+++ src/modules/mini-cart/mini-cart.js
@@ -1,13 +1,28 @@
 import select from 'select-dom'
+import nano from 'nanoajax'
 import on from 'dom-event'
 
 const nav = select('#nav')
 const btn = select('.js-cart-btn', nav)
 
+const getMiniCart = (cart) => {
+  let table = select('.js-table', cart)
+  return new Promise((resolve, reject) => {
+    nano.ajax(
+      {url: `/cart?view=mini`},
+      (code, res) => {
+        cart.classList.add('is-loaded')
+        table.innerHTML = res.trim()
+      }
+    )
+  })
+}
+
 export default (el) => {
   let cart = el
   on(btn, 'mouseenter', () => {
     cart.classList.add('is-active')
+    getMiniCart(cart)
   })
   on(cart, 'mouseleave', () => {
     cart.classList.remove('is-active')
